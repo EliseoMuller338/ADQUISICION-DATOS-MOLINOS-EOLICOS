75ef785c27e1b220f19c795688d639544f6471c4
Velazquez Eduardo <eduardo.antonio.velazquez@gmail.com>
2022-06-07 09:53:53 -0300

    Update programa_molinos_eolicos_1.ino
    Incorporación descripción

--- programa_molinos_eolicos_1/programa_molinos_eolicos_1.ino
+++ programa_molinos_eolicos_1/programa_molinos_eolicos_1.ino
@@ -1,3 +1,22 @@
+/*proyecto GELACC adquisición de datos molino
+ * V1.0.0 al 31-05-2022
+ * ********************************************
+ * Medición de tensión
+ * - Cada 1 minuto
+ * - Filtro de mediciones 1 muestra cada 10 ms.
+ * - Registro en SD 
+ * ********************************************
+ * Medición de frecuencia
+ * - Cada 1 minuto
+ * - Filtro de mediciones (conteo de pulsos durante 1 minuto Frecuencia=conteoDePulsos/60)
+ * - Registro en SD
+ * - Funcionamiento por interrupción Externa por flanco descendente.
+ * Fecha y Hora
+ * - Uso de funciones tomadas del ejemplo de RTClib.h en manejoCLock.h
+ * - Configuración RTC
+ * Manejo SD
+ * - Funciones en manejoSD (configuración y almacenamiento de datos) 
+ */
 float B=0;
 float C=0;
 float A=0;
